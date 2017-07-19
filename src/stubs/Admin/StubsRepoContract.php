13a0c9cbd9ce2dae585f7f5b4ce07a37eb75d263
Michael Tintiuc <overcrookd@gmail.com>
2017-07-19 23:48:33 +0300

    Added repo contract stub

---
+++ src/stubs/Admin/StubsRepoContract.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Components\Stubs\Admin;
+
+use MichaelT\Component\Admin\Contracts\RepoContract;
+use MichaelT\Component\Admin\Contracts\SearchableContract;
+
+interface StubsRepoContract extends RepoContract, SearchableContract
+{
+}
